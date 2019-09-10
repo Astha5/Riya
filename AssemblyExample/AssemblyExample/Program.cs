f38fb88889e92614e44f22df5f53dea5e7508a75
Astha5 <astha05101997@gmail.com>
2019-09-10 16:46:00 +0530

    Add files via upload

---
+++ AssemblyExample/AssemblyExample/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using SampleNamespace;
+
+namespace AssemblyExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            Customer customer = new Customer();
+           // customer.GetCustomerNAmeUpperCase();
+            //Console.WriteLine("Enter Customer NAme");
+            customer.CustomerName = "Astha";
+            Console.WriteLine(customer.GetCustomerNAmeUpperCase());
+            customer.CustomerId = 3024;
+            Console.WriteLine(customer.CustomerId);
+            //Reflection:obtaining types of assembly
+            Assembly assembly = Assembly.GetAssembly(typeof(Customer));
+            Console.WriteLine("Full Name:" + assembly.FullName);
+            Console.WriteLine("Code Base:" + assembly.CodeBase);
+
+            Type[] classes = assembly.GetTypes();
+            foreach (Type cls in classes)
+            {
+                Console.WriteLine(cls.Name);
+                Console.WriteLine(cls.Name);
+                Console.WriteLine("\n Properties:");
+                foreach (PropertyInfo prop in cls.GetProperties())
+                {
+                    Console.WriteLine(prop);
+                }
+                Console.WriteLine("\n Methods:");
+                foreach (MethodInfo meth in cls.GetMethods())
+                {
+                    Console.WriteLine(meth);
+                }
+                Console.WriteLine("\n Fields:");
+                foreach (FieldInfo fie in cls.GetFields())
+                {
+                    Console.WriteLine(fie);
+                }
+                Console.WriteLine("\n Attributes:");
+                foreach (Attribute atr in cls.GetCustomAttributes())
+                {
+                    Console.WriteLine(atr);
+                }
+            }
+            Console.ReadKey();
+        
+
+        }
+
+    }
+}
---
+++ AssemblyExample/ClassLibrary1/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SampleNamespace
+{
+    public class SampleAttribute : Attribute
+    {
+        public SampleAttribute()
+
+        {
+            Console.WriteLine("Constructer is called");
+        }
+
+    }
+    [Sample]
+    [Serializable]
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string GetCustomerNAmeUpperCase()
+        {
+            Type type = typeof(Customer);
+            if (type.GetCustomAttribute(typeof(SampleAttribute)) != null)
+                return CustomerName.ToUpper();
+            else
+                return CustomerName;
+
+        }
+        public int XYZ()
+        {
+            return 4;
+        }
+    }
+}
