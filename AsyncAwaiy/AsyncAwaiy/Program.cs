564731feb43a8a789d28f8c0799208f1631b0cb5
Astha5 <astha05101997@gmail.com>
2019-09-12 16:15:11 +0530

    Add files via upload

---
+++ AsyncAwaiy/AsyncAwaiy/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncAwaiy
+{
+    class Sample
+    {
+        public async Task<long> SumofNatutalNumbers(int n)
+        {
+            long sum=0;
+            await Task.Run(()=> {
+                for (int i = 1; i <= n; i++)
+                {
+                    sum = sum + i;
+                }
+            });
+            
+            return sum;
+        }
+    }
+    class Program
+    {
+        static async Task Main()
+        {
+            Sample sample = new Sample();
+            long s=await sample.SumofNatutalNumbers(5);
+            Console.WriteLine("Continue------------");
+            Console.WriteLine($"sum is {s}");
+            Console.ReadKey();
+
+        }
+    }
+}
