91302174efef92e45558dfbf4a8216ace7d0bcbe
Astha5 <astha05101997@gmail.com>
2019-09-16 10:41:58 +0530

    Add files via upload

---
+++ Supplier_Astha/Inventory.DataAccessLayer/SupplierDAL.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Inventory.Entities;
+
+
+namespace Inventory.DataAccessLayer
+{
+    public class SupplierDAL
+    {
+        //this is a service class....adding of entries
+        //add list if validated
+        public static List<Supplier> SupplierList = new List<Supplier>();
+
+        public bool AddSupplierDAL(Supplier newSupplier)
+        {
+            bool SupplierAdded = false;
+            try
+            {
+                SupplierList.Add(newSupplier);
+                SupplierAdded = true;
+            }
+            catch (SystemException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return SupplierAdded;
+
+        }
+
+        public List<Supplier> GetAllSupplierDAL()
+        {
+            return SupplierList;
+        }
+
+        public Supplier SearchSupplierDAL(int searchSupplierId)
+        {
+            Supplier searchSupplier = null;
+            try
+            {
+                foreach(Supplier item in SupplierList)
+                {
+                    if ((item.SupplierId) .Equals (searchSupplierId))
+                    {
+                        searchSupplier = item;
+                    }
+                }
+            }
+            catch (SystemException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return searchSupplier;
+        }
+
+        public List<Supplier> GetSuppliersByNameDAL(string SupplierName)
+        {
+            List<Supplier> searchSupplier = new List<Supplier>();
+            try
+            {
+                foreach (Supplier item in SupplierList)
+                {
+                    if (item.SupplierName == SupplierName)
+                    {
+                        searchSupplier.Add(item);
+                    }
+                }
+            }
+            catch (SystemException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return searchSupplier;
+        }
+
+        public bool UpdateSupplierDAL(Supplier updateSupplier)
+        {
+            bool SupplierUpdated = false;
+            try
+            {
+                for (int i = 0; i < SupplierList.Count; i++)
+                {
+                    if (SupplierList[i].SupplierId == updateSupplier.SupplierId)
+                    {
+                        updateSupplier.SupplierName = SupplierList[i].SupplierName;
+                        updateSupplier.SupplierPhone = SupplierList[i].SupplierPhone;
+                        updateSupplier.SupplierEmail = SupplierList[i].SupplierEmail;
+                        
+                        SupplierUpdated = true;
+                    }
+                }
+            }
+            catch (SystemException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return SupplierUpdated;
+
+        }
+
+        public bool DeleteSupplierDAL(int deleteSupplierId)
+        {
+            bool SupplierDeleted = false;
+            try
+            {
+                Supplier deleteSupplier = null;
+                foreach (Supplier item in SupplierList)
+                {
+                    if ((item.SupplierId) .Equals (deleteSupplierId))
+                    {
+                        deleteSupplier = item;
+                    }
+                }
+
+                if (deleteSupplier != null)
+                {
+                    SupplierList.Remove(deleteSupplier);
+                    SupplierDeleted = true;
+                }
+            }
+            catch (DbException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return SupplierDeleted;
+
+        }
+       
+    }
+}
---
+++ Supplier_Astha/Inventory.Entities/SupplierEntities.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Entities
+{
+    
+        public class Supplier
+        {
+
+        private string _supplierName;
+        public string SupplierName
+        {
+            get { return _supplierName; }
+            set { _supplierName = value; }
+        }
+
+        private string _supplierId;
+        public string SupplierId
+        {
+            get { return _supplierId; }
+            set { _supplierId = value; }
+        }
+
+        private string _supplierPhone;
+        public string SupplierPhone
+        {
+            get { return _supplierPhone; }
+            set { _supplierPhone = value; }
+        }
+
+        private string _supplierEmail;
+        public string SupplierEmail
+        {
+            get { return _supplierEmail; }
+            set { _supplierEmail = value; }
+        }
+        public Supplier()
+        {
+            _supplierName = null;
+            _supplierId = null;
+            _supplierEmail = null;
+            _supplierPhone = null;
+
+        }
+
+
+        //validate normally
+    }
+    
+
+
+}
